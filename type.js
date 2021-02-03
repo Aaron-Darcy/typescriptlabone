22515ea605a904a00a8fb3fd681ffff93a8deb28
Aaron Darcy <g00373146@gmit.ie>
2021-02-03 15:08:05 +0000

    lab upload

---
+++ type.js
@@ -0,0 +1,10 @@
+var myNumber = 55;
+console.log("MyNumber is: " + myNumber);
+var color = "hello world";
+console.log(color);
+var myBool = false;
+console.log(myBool);
+var listb = ["Hello", "3", "false"];
+for (var i = 0; i < listb.length; i++) {
+    console.log("String " + (i + 1) + " is " + listb[i]);
+}
---
+++ type.ts
@@ -0,0 +1,13 @@
+let myNumber: number = 55;
+console.log("MyNumber is: " + myNumber);
+
+let color: string = "hello world"
+console.log(color);
+
+let myBool: boolean = false;
+console.log(myBool);
+
+let listb: Array<string> = ["Hello", "3", "false"]
+for (let i = 0; i < listb.length; i++) {
+    console.log("String " + (i + 1) + " is " + listb[i]);
+}
